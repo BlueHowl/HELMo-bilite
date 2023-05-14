024d6bcb95d8d5889fe56dc3dd5a00d70888a836
Valentin Lopez Lopez <v.lopezlopez@student.helmo.be>
2023-05-14 21:34:18 +0200

    implementation de la modification des donne pour les driver

--- Models/Driver.cs
+++ Models/Driver.cs
@@ -5,7 +5,7 @@ namespace HELMo_bilite.Models;
 public class Driver : HelmoMember
 {
     
-    public ICollection<License> Licenses { get; set; } = new List<License>();
+    public ICollection<License>? Licenses { get; set; }
 
     public ICollection<Delivery>? Deliverys { get; set; } = new List<Delivery>();
 
